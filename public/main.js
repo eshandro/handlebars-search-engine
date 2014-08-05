2d2c8b663b64e5c98a860d3baeb9f8d32fac0819
Erik Shandrowski <erik843@gmail.com>
2014-08-05 16:42:53 -0600

    Done including bonus using clientjade

--- public/main.js
+++ public/main.js
@@ -1,12 +1,14 @@
 $(document).on('ready', function() {
-	var resultsTemplateSource = $('#results').html();
-	var resultsTemplate = Handlebars.compile(resultsTemplateSource);
+/*	var resultsTemplateSource = $('#results').html();
+	var resultsTemplate = Handlebars.compile(resultsTemplateSource);*/
 
 	$(document).on('submit', '#search-form', function(e) {
 		e.preventDefault();
-		$.get('/search', { searchTerm: $('input[name="searchTerm"]').val() }, function(answer) {
+		$.get('/search', { searchTerm: $('input[name="searchTerm"]').val() }, 
+			function(answer) {
 			$('.results').empty();
-			$('.results').append(resultsTemplate({answer: answer}));
+			var jadeEl = $('.results');
+			jade.render(jadeEl.get(0), 'search', {results: answer});
 		})
 	});
 
